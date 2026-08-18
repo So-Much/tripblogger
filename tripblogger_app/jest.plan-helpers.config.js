4f7232e23a45f42e12d73f078cfb2e69a986bfcb
SoMuch <nhieu.luu@nvsoft.vn>
2026-08-18 10:22:34 +0700

    plan(app): jet config

---
+++ tripblogger_app/jest.plan-helpers.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src/features/trip/plan'],
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+};
